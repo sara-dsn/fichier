2a7290644c5d70449160572e257deb208bfe2b38
sara-dsn <sabr.tdg@gmail.com>
2024-03-22 12:24:49 +0100

    pour tout recuperer avant nettoyage

---
+++ artist.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.1deb5ubuntu1
+ */
+
+/**
+ * Database `velvet`
+ */
+
+/* `velvet`.`artist` */
+$artist = array(
+  array('id' => '1','name' => 'Neil Young','url' => 'bientôt disponible'),
+  array('id' => '2','name' => 'YES','url' => 'bientôt disponible'),
+  array('id' => '3','name' => 'Rolling Stones','url' => 'bientôt disponible'),
+  array('id' => '4','name' => 'Blue Metal','url' => 'bientôt disponible'),
+  array('id' => '5','name' => '80','url' => 'bientôt disponible'),
+  array('id' => '6','name' => 'AC/DC','url' => 'bientôt disponible'),
+  array('id' => '7','name' => '0','url' => 'bientôt disponible'),
+  array('id' => '8','name' => 'Bob Dylan','url' => 'bientôt disponible'),
+  array('id' => '9','name' => 'Fleshtones','url' => 'bientôt disponible'),
+  array('id' => '10','name' => 'The Clash','url' => 'bientôt disponible'),
+  array('id' => '13','name' => 'Queens of The stone Age','url' => 'https://qotsa.com')
+);
